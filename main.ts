590ba38640b753a91f62ab1ec4ac52f6957c3adb
misike12 <59843249+misike12@users.noreply.github.com>
2023-02-21 11:55:03 +0100

    Kezdőfájlok a MakeCode projekthez

---
+++ main.ts
@@ -0,0 +1,183 @@
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showLeds(`
+            . . # . .
+            # # # # .
+            . . # . #
+            . # . # .
+            . # . # .
+            `)
+        basic.showLeds(`
+            . . # . .
+            # # # # #
+            . . # . .
+            . # . # .
+            . # . # .
+            `)
+        basic.showLeds(`
+            . . # . #
+            # # # # .
+            . . # . .
+            . # . # .
+            . # . # .
+            `)
+        basic.showLeds(`
+            . . # . .
+            # # # # #
+            . . # . .
+            . # . # .
+            . # . # .
+            `)
+        basic.showLeds(`
+            . . # . .
+            # # # # .
+            . . # . #
+            . # . # .
+            . # . # .
+            `)
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        # . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . # . . .
+        # . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . # . . .
+        # . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . # .
+        . . # . .
+        . # . . .
+        # . . . .
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . . # .
+        . . # . .
+        . # . . .
+        # . . . .
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . . # .
+        . . # . .
+        . # . # .
+        # . . . .
+        `)
+    basic.showLeds(`
+        . . . . #
+        . . . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . . # . .
+        `)
+    basic.showLeds(`
+        # . . . #
+        . # . # .
+        . . # . .
+        . # . # .
+        # . . . #
+        `)
+    basic.showLeds(`
+        . . # . .
+        . . # . .
+        # # # # #
+        . . # . .
+        . . # . .
+        `)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+        . . # . .
+        . # # # .
+        # . # . #
+        . # . # .
+        . # . # .
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . # . #
+        . # . # .
+        . # . # .
+        . . . . .
+        `)
+    basic.showLeds(`
+        # . # . #
+        . # # # .
+        . # . # .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . # . # .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
